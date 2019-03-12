884ce0753b440fb55813d3e1feeba555518d164d
Dalcoleas <00048015@uca.edu.sv>
2019-03-12 10:37:32 -0600

    Agregando datos

--- app/src/main/java/com/dalcoleas/laboratorio1/MainActivity.java
+++ app/src/main/java/com/dalcoleas/laboratorio1/MainActivity.java
@@ -3,6 +3,13 @@ package com.dalcoleas.laboratorio1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+* Estudiante: Daniel Alcoleas
+* Carnet: 00048015
+* Curso: Programación de dispositivos móviles
+* Ciclo: 01/19
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
